62cadc1eb94acdd06cc7d5d17ec07d18e2d2e1dd
Nicholas Maltbie <nick.dmalt@gmail.com>
2016-11-19 23:41:21 -0500

    Fixed footer position for the computer screen

--- js/padding.js
+++ js/padding.js
@@ -1,4 +1,6 @@
 function alightPaddingForNav() {
     var height = document.getElementById("nav").clientHeight;
     document.body.style.padding = height + " 0 0 0";
+    var heightFoot = document.getElementById("footer").clientHeight;
+    document.getElementById("content-holder").style.padding = "0 0 " + heightFoot + " 0";
 }
